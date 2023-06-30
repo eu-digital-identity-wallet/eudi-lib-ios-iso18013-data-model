0afdf91ca999e66e370669a6c684d9d784646610
Filippos Sakellaropoulos <phisakel@gmail.com>
2023-06-30 11:15:56 +0300

    ARF v1.2.0 changes

--- Sources/MdocDataModel18013/MdocKnownDocTypes/EuPidModel.swift
+++ Sources/MdocDataModel18013/MdocKnownDocTypes/EuPidModel.swift
@@ -10,15 +10,11 @@ public struct EuPidModel: Codable, MdocDecodable {
 	public static let title = String.LocalizationValue("eu_pid_doctype_name")
 	
 	public let family_name: String?
-	public let family_name_national_characters: String?
 	public let given_name: String?
-	public let given_name_national_characters: String?
 	public let birth_date: String?
 	public let unique_id: String?
 	public let family_name_birth: String?
-	public let family_name_birth_national_characters: String?
 	public let given_name_birth: String?
-	public let given_name_birth_national_characters: String?
 	public let birth_place: String?
 	public let birth_country: String?
 	public let birth_state: String?
@@ -28,13 +24,15 @@ public struct EuPidModel: Codable, MdocDecodable {
 	public let resident_postal_code: String?
 	public let resident_state: String?
 	public let resident_country: String?
+	public let resident_street: String?
+	public let resident_house_number:String?
 	public let gender: UInt64?
 	public let nationality: String?
 	public let age_in_years: UInt64?
 	public let age_birth_year: UInt64?
-	public let issue_date: String?
 	public let expiry_date: String?
 	public let issuing_authority: String?
+	public let issuance_date: String?
 	public let document_number: String?
 	public let administrative_number: String?
 	public let issuing_country: String?
@@ -42,15 +40,11 @@ public struct EuPidModel: Codable, MdocDecodable {
 	
 	public enum CodingKeys: String, CodingKey, CaseIterable {
 		case family_name
-		case family_name_national_characters
 		case given_name
-		case given_name_national_characters
 		case birth_date
 		case unique_id
 		case family_name_birth
-		case family_name_birth_national_characters
 		case given_name_birth
-		case given_name_birth_national_characters
 		case birth_place
 		case birth_country
 		case birth_state
@@ -60,13 +54,15 @@ public struct EuPidModel: Codable, MdocDecodable {
 		case resident_postal_code
 		case resident_state
 		case resident_country
+		case resident_street
+		case resident_house_number
 		case gender
 		case nationality
 		case age_in_years
 		case age_birth_year
-		case issue_date
 		case expiry_date
 		case issuing_authority
+		case issuance_date
 		case document_number
 		case administrative_number
 		case issuing_country
@@ -83,15 +79,11 @@ extension EuPidModel {
 		Self.extractAgeOverValues(dict, &ageOverXX)
 		Self.extractDisplayStrings(items, &displayStrings)
 		family_name = getValue(key: .family_name)
-		family_name_national_characters = getValue(key: .family_name_national_characters)
 		given_name = getValue(key: .given_name)
-		given_name_national_characters = getValue(key: .given_name_national_characters)
 		birth_date = getValue(key: .birth_date)
 		unique_id = getValue(key: .unique_id)
 		family_name_birth = getValue(key: .family_name_birth)
-		family_name_birth_national_characters = getValue(key: .family_name_birth_national_characters)
 		given_name_birth = getValue(key: .given_name_birth)
-		given_name_birth_national_characters = getValue(key: .given_name_birth_national_characters)
 		birth_place = getValue(key: .birth_place)
 		birth_country = getValue(key: .birth_country)
 		birth_state = getValue(key: .birth_state)
@@ -101,13 +93,15 @@ extension EuPidModel {
 		resident_postal_code = getValue(key: .resident_postal_code)
 		resident_state = getValue(key: .resident_state)
 		resident_country = getValue(key: .resident_country)
+		resident_street = getValue(key: .resident_street)
+		resident_house_number = getValue(key: .resident_house_number)
 		gender = getValue(key: .gender)
 		nationality = getValue(key: .nationality)
 		age_in_years = getValue(key: .age_in_years)
 		age_birth_year = getValue(key: .age_birth_year)
-		issue_date = getValue(key: .issue_date)
 		expiry_date = getValue(key: .expiry_date)
 		issuing_authority = getValue(key: .issuing_authority)
+		issuance_date = getValue(key: .issuance_date)
 		document_number = getValue(key: .document_number)
 		administrative_number = getValue(key: .administrative_number)
 		issuing_country = getValue(key: .issuing_country)
