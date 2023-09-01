ea17840d8f8d50a09a3c21dd97f4fd451b914d39
Filippos Sakellaropoulos <phisakel@gmail.com>
2023-09-01 20:48:59 +0300

    issuer signed mandatory, signup response extensions for keys

--- Sources/MdocDataModel18013/MdocResponse/IssuerSigned.swift
+++ Sources/MdocDataModel18013/MdocResponse/IssuerSigned.swift
@@ -7,14 +7,14 @@ import SwiftCBOR
 /// Contains the mobile security object for issuer data authentication and the data elements protected by issuer data authentication.
 public struct IssuerSigned {
 	public let issuerNameSpaces: IssuerNameSpaces?
-	public let issuerAuth: IssuerAuth? //todo: make mandatory
+	public let issuerAuth: IssuerAuth
 	
 	enum Keys: String {
 	   case nameSpaces
 	   case issuerAuth
 	 }
 	
-	public init(issuerNameSpaces: IssuerNameSpaces? = nil, issuerAuth: IssuerAuth? = nil) {
+	public init(issuerNameSpaces: IssuerNameSpaces?, issuerAuth: IssuerAuth) {
 		self.issuerNameSpaces = issuerNameSpaces
 		self.issuerAuth = issuerAuth
 	}
@@ -24,8 +24,7 @@ extension IssuerSigned: CBORDecodable {
 	public init?(cbor: CBOR) {
 		guard case let .map(m) = cbor else { return nil }
 		if let cn = m[Keys.nameSpaces] { issuerNameSpaces = IssuerNameSpaces(cbor: cn) } else { issuerNameSpaces = nil }
-		//guard let cia = m[Keys.issuerAuth], let ia = IssuerAuth(cbor: cia) else { return nil }; issuerAuth = ia
-		if let cia = m[Keys.issuerAuth], let ia = IssuerAuth(cbor: cia) { issuerAuth = ia } else { issuerAuth = nil }
+		guard let cia = m[Keys.issuerAuth], let ia = IssuerAuth(cbor: cia) else { return nil }; issuerAuth = ia
 	}
 }
 
@@ -33,7 +32,7 @@ extension IssuerSigned: CBOREncodable {
 	public func toCBOR(options: CBOROptions) -> CBOR {
 		var cbor = [CBOR: CBOR]()
 		if let ns = issuerNameSpaces { cbor[.utf8String(Keys.nameSpaces.rawValue)] = ns.toCBOR(options: options) }
-		if let ia = issuerAuth { cbor[.utf8String(Keys.issuerAuth.rawValue)] = ia.toCBOR(options: options) }
+		cbor[.utf8String(Keys.issuerAuth.rawValue)] = issuerAuth.toCBOR(options: options)
 		return .map(cbor)
 	}
 }
