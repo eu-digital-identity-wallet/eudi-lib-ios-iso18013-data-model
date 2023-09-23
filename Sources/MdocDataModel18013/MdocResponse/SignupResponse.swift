20e33a2620ee5a4909c0dbd1305d358cc18aaa0f
Filippos Sakellaropoulos <phisakel@gmail.com>
2023-09-23 23:33:03 +0300

    Update SignupResponse.swift

--- Sources/MdocDataModel18013/MdocResponse/SignupResponse.swift
+++ Sources/MdocDataModel18013/MdocResponse/SignupResponse.swift
@@ -4,29 +4,25 @@ import SwiftCBOR
 
 /// Signup response encoded as base64
 public struct SignUpResponse: Codable {
-	public let data: String
+	public let data: String?
+	public let response: String?
+	public let pin: String
 	public let privateKey: String?
 	public let publicKey: String?
 	public var deviceResponse: DeviceResponse? {
-		guard let d = Data(base64Encoded: data) else { return nil }
+		guard let b64 = data ?? response, let d = Data(base64Encoded: b64) else { return nil }
 		return DeviceResponse(data: d.bytes)
 	}
-	public var deviceResponseBase64URLencoded: DeviceResponse? {
-		guard let d = Data(base64URLEncoded: data) else { return nil }
-		return DeviceResponse(data: d.bytes)
-	}
+
 	public var devicePrivateKey: CoseKeyPrivate? {
 		guard let privateKey else { return nil }
 		return CoseKeyPrivate(base64: privateKey)
 	}
 	
-	public var devicePublicKey: CoseKey? {
-		guard let publicKey, let d = Data(base64Encoded: publicKey) else { return nil }
-		return CoseKey(data: [UInt8](d))
-	}
-	
 	enum CodingKeys: String, CodingKey {
 		case data = "Data"
+		case response
+		case pin
 		case privateKey = "PrivateKey"
 		case publicKey = "PublicKey"
 	}
