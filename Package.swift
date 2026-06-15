0a972bd72f39cc93c51c6fcaf6a50e46d2648748
Filippos Sakellaropoulos <phisakel@gmail.com>
2026-06-15 23:45:36 +0300

    Remove outdated dependency on swift-docc-plugin from Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,6 @@ let package = Package(
             targets: ["MdocDataModel18013"]),
     ],
     dependencies: [
- 	    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
         .package(url: "https://github.com/niscy-eudiw/SwiftCBOR.git", from: "0.6.4"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.1"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.10.1"),
